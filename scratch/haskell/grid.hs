6e4bcea882a9c2b720545b842ff257254ea8d524
hakim <hakim.cassimally@gmail.com>
2009-03-10 00:44:58 +0000

    Added haskell grid.hs to scratch

---
+++ scratch/haskell/grid.hs
@@ -0,0 +1,29 @@
+
+-- we want to fold something like this
+g = [
+    [ 1, 2, 3 ],
+    [ 4, 5, 6 ],
+    [ 7, 8, 9 ]
+    ]
+
+-- into a Cell { value=1, right=(the Cell with value 2), down=(the cell with value 4) }
+
+data Cell a = Cell {
+    value :: a,
+    down  :: Cell a,
+    right :: Cell a
+    }
+    | Nil
+    deriving Show
+
+right' :: Cell a -> Cell a
+right' c@(Cell {}) = right c
+right' Nil         = Nil
+
+zipRow :: [a] -> Cell a -> Cell a
+zipRow xs c = foldr mkCell Nil valAndDown
+    where valAndDown = zip xs (iterate right' c)
+          mkCell (v,d) r = Cell { value=v, down=d, right=r }
+
+zipGrid :: [[a]] -> Cell a
+zipGrid = foldr zipRow Nil
