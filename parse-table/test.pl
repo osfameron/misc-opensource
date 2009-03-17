88bcb787e653179971255db0227cee6457377bdf
hakim <hakim.cassimally@gmail.com>
2009-03-17 23:25:43 +0000

    Date expansion

--- parse-table/test.pl
+++ parse-table/test.pl
@@ -2,18 +2,38 @@
 
 package Row::Name;
 use Fixed;
-use Fixed::Row;
 extends 'Fixed::Row';
+
+use DateTime::Format::DateParse;;
+
+use Params::Coerce ();
+use Moose::Util::TypeConstraints;
+
+subtype 'My.DateTime' =>
+    as class_type('DateTime');
+
+coerce 'My.DateTime' 
+    => from 'Str'
+        => via { 
+                 my $date = DateTime::Format::DateParse->parse_datetime( $_ );
+                 warn "Gots $date ($_)";
+                 $date; };
 
 column first => range  => [0, 4 ];
 column last  => range  => [5, 11];
+column date  => range  => [12, 22], isa => 'My.DateTime', coerce => 1;
 
 package main;
 
 use Test::More tests => 3;
 
-my $obj = Row::Name->parse('Fred Bloggs');
+my $obj = Row::Name->parse('Fred Bloggs 2009-03-17');
 
 isa_ok $obj, 'Row::Name';
 is $obj->first, 'Fred';
 is $obj->last,  'Bloggs';
+isa_ok $obj->date, 'DateTime';
+
+use Data::Dumper;
+local $Data::Dumper::Maxdepth=1; local $Data::Dumper::Indent = 1;
+diag Dumper($obj);
