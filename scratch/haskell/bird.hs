9406a97ebb6775410345e8e24883d1163fd64d6c
Hakim Cassimally <hakim@eschew.local>
2011-05-17 11:17:07 +0100

    Parser for Smullyan's combinator birds

---
+++ scratch/haskell/bird.hs
@@ -0,0 +1,56 @@
+import Text.ParserCombinators.Parsec hiding (many, (<|>), optional)
+import Control.Applicative
+
+data Bird = Bird  String
+          | BirdV String
+          | Song [Bird]
+
+instance Show Bird where
+    show x = "unsafeBirdFromString " ++ (show $ show' x)
+        where show' (Bird  x) =  x
+              show' (BirdV x) = x
+              show' (Song xs) = concatMap show'' xs
+              show'' s@(Song  _) = "(" ++ (show' s) ++ ")"
+              show'' b           = show' b
+
+simplify :: Bird -> Bird
+
+simplify (Song [x])           = simplify x
+simplify (Song ((Song x):xs)) = Song $ map simplify $ x ++ xs
+simplify (Song xs)            = Song $ map simplify xs
+simplify b                    = b
+
+birdFromString source = case (parse parseExprOuter "" source) of
+    Right x -> return $ simplify x
+    Left  e -> error $ show e
+
+unsafeBirdFromString source = case (parse parseExprOuter "" source) of
+    Right x -> simplify x
+    Left  e -> error $ show e
+
+parseExprOuter = ((try parseSong) <|> parseExprAux) <* eof
+
+parseExprAux   = (try parseList) <|> (try parseBird)  <|> (try parseBirdV)
+
+parseBirdV = do f <- lower 
+                r <- many (digit <|> oneOf "*^")
+                return $ BirdV (f:r)
+
+parseBird = do f <- upper 
+               r <- many (digit <|> oneOf "*^")
+               return $ Bird (f:r)
+
+parseList = char '(' *> parseSong <* char ')'
+
+parseSong = do
+    x <- many1 parseExprAux
+    return $ Song x
+
+matchSong :: Bird -> Bird -> Maybe Bird
+matchSong (BirdV x) y = Just y
+matchSong (Bird  x) b@(Bird y) | x == y = Just b
+matchSong (Song xs) s@(Song ys) | length xs == length ys
+    = case sequence $ zipWith matchSong xs ys of
+        Nothing -> Nothing
+        Just _  -> Just s
+matchSong _ _ = Nothing
